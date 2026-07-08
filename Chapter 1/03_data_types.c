80d9b7798ec3e6c750259aacad12aac31e4c730f
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-08 18:07:07 +0530

    Data types

---
+++ Chapter 1/03_data_types.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a;
+    // a = 1;
+
+    int a = 1;
+
+    // float b = 1.4;
+    float b;
+    b = 1.4;
+
+    char c = 'K';
+
+    printf("The value of a is: %d\n", a);
+    printf("The value of b is: %f\n", b);
+    printf("The value of c is: %c\n", c);
+
+
+    return 0;
+}
+
+// % d for integer
+// % f for float
+// $ c for character
