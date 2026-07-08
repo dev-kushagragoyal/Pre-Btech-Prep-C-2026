56afba422303c61e820f56e51116c1a1114f05d9
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-08 18:07:45 +0530

    Comments

---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // I already know how to use comments
+    // As I aleady know pyhton
+
+    /*I am multiple line comment
+    Yes this is a multiple line
+    Keep It up!*/
+
+    int a;
+    a = 22;
+    printf("The value of a is: %d", a);
+    return 0;
+}
