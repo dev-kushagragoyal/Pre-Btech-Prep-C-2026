6fce755817dfa21526123b5d24cf7314701b9b0b
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-09 13:20:59 +0530

    Type Conversions

---
+++ Chapter 2/03_type_conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 9;
+    float b = 2;
+    float c = a / b;
+    int d = 3;
+    int e = 9;
+    float answer = d / e;
+    printf("The value is: %f\n", c);
+    printf("The value of is: %f\n", answer);
+    return 0;
+} 
