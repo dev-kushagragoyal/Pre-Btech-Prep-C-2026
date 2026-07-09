ac930c871630820fe819af69fd7eef72a7ac0a4c
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-09 14:43:28 +0530

    Solved Problem 05
    Wrap up Chapter 02

---
+++ Chapter 2/Practice Set/Problem_05.c
@@ -0,0 +1,16 @@
+// 5. 3.0 + 1 will be:
+// a. Integer.
+// b. Floating point number.
+// c. Character.
+
+// Answer = (b)
+
+#include <stdio.h>
+
+int main()
+{
+    float a = 3.0;
+    int b = 1;
+    printf("The final answer is: %f",a + b);
+    return 0;
+}
