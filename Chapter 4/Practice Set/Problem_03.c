ba69dba9e5ad382a14135022ce240c7c97369f54
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-10 14:38:47 +0530

    Solved Problem 03

---
+++ Chapter 4/Practice Set/Problem_03.c
@@ -0,0 +1,14 @@
+// 3. A do while loop is executed:
+// a. At least once.
+// b. At least twice.
+// c. At most once.
+
+// Answer = (a)
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
