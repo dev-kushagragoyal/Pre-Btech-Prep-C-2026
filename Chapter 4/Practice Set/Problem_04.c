0fd18391b9235404b0d2e9d13f5b6466cb559833
Kushagra Goyal <dev.kushagragoyal@gmail.com>
2026-07-10 14:38:59 +0530

    Solved Problem 04

---
+++ Chapter 4/Practice Set/Problem_04.c
@@ -0,0 +1,12 @@
+// 4. What can be done using one type of loop, can also be done using the other two
+// types of loops – true or false?
+
+// Answer = True
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
